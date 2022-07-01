7aba8ed09f3360c9d960524f7bd90d8b525c997b
WilsonVinson <cprewilsonvinson@gmail.com>
2022-07-01 22:31:57 +0800

    main

--- core/tilemap_manager.py
+++ core/tilemap_manager.py
@@ -1,12 +1,15 @@
 
 import random
-import noise
+import re
 
+import noise
 import pygame
 
 class TilemapManager:
-    def __init__(self,map):
+    def __init__(self,map,tile_land,tile_building):
         self.map = map
+        self.tile_land = tile_land
+        self.tile_building = tile_building
         
         self.tile_width = 64
         
@@ -28,21 +31,34 @@ class TilemapManager:
         tilemap = [[int(noise.pnoise2((x/freq)+seed,(y/freq)+seed,octaves)*100+50) for x in range(0,boarder,1)] for y in range(0,boarder,1)]
 
     def renderer(self,surface):
-        for tile_list in self.assets:
-            for row in range(len(self.map['land'])): # 行
-                for column in range(len(self.map['land'][row])):
-                    tile_rect = self.assets[tile_list][self.map[tile_list][row][column]].get_rect()
-                    tile_rect.x = column*(self.tile_width/2)-row*(self.tile_width/2)+self.pos[0]
-                    if tile_list == 'land':
-                        tile_rect.y = row*(self.tile_width/4)+column*(self.tile_width/4)+self.pos[1]
-                        self.touch_rect_map[row][column]  = tile_rect
-                    if tile_list == 'building':
-                        tile_rect.y = row*(self.tile_width/4)+column*(self.tile_width/4)+self.pos[1]-self.tile_width/2
-                    surface.blit(self.assets[tile_list][self.map[tile_list][row][column]], tile_rect)
-                
-    def timer(self):
-        pass
+        for tile_list in self.assets: # land, building
+            for row in range(len(self.map['land'])): # row行
+                for column in range(len(self.map['land'][row])): # column列
+                    tile_rect = self.assets[tile_list][self.map[tile_list][row][column]].get_rect() # 获取图片的矩形
+                    tile_rect.x = column*(self.tile_width/2)-row*(self.tile_width/2)+self.pos[0] # 设置矩形的x坐标
+                    if tile_list == 'land': # 如果是地面
+                        tile_rect.y = row*(self.tile_width/4)+column*(self.tile_width/4)+self.pos[1] # 设置矩形的y坐标
+                        self.touch_rect_map[row][column]  = tile_rect # 设置矩形的y坐标
+                    if tile_list == 'building': # 如果是建筑
+                        tile_rect.y = row*(self.tile_width/4)+column*(self.tile_width/4)+self.pos[1]-self.tile_width/2 # 设置矩形的y坐标
+                    surface.blit(self.assets[tile_list][self.map[tile_list][row][column]], tile_rect) # 绘制图片
     
+    def timer(self):
+        # for key in self.tile_building:
+        #     print(self.tile_building[key]['timer'])
+        
+        for row in range(len(self.map['building'])): # row行
+            for column in range(len(self.map['building'][row])): # column列
+            
+                # timer
+                if self.map['timer'][row][column] == 0:
+                    self.map['timer'][row][column] += 1
+                if self.map['timer'][row][column] == self.tile_building[str(self.map['building'][row][column])]['timer']:
+                    self.map['building'][row][column] += 1
+                print(self.map['building'])
+        
+        return self.map
+        
     def touch(self):
         pass
     
--- main.py
+++ main.py
@@ -16,12 +16,14 @@ pygame.mixer.init()
 font_list = [pygame.font.Font('assets/font/LockClock.ttf', size)for size in range(0,(64+1),1)]
 
 window_size = (1280,720)
-window = pygame.display.set_mode(window_size)#pygame.RESIZABLE
+window = pygame.display.set_mode(window_size) # pygame.RESIZABLE
 window_title = pygame.display.set_caption('TinyLand')
 window_icon = pygame.display.set_icon(pygame.image.load('assets/land/land1.png'))
 window_clock = pygame.time.Clock()
 
 RUN = True
+
+
 
 while RUN == True:
     window.fill((0,0,0,0))
@@ -32,10 +34,13 @@ while RUN == True:
             
     tilemap_surface = pygame.Surface(window_size).convert_alpha()
     tilemap_surface.fill((0,0,0,0))
-    tm = core.tilemap_manager.TilemapManager(map)
+    
+    tm = core.tilemap_manager.TilemapManager(map_01,tile_land,tile_building)
     tm.renderer(tilemap_surface)
+    map_01 = tm.timer()
     
     window.blit(tilemap_surface, (0,0))
     
     pygame.display.update()
     window_clock.tick(60)
+    
