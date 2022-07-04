ef3984d12b0476cd2b94a66c68070c22e73df505
WilsonVinson <cprewilsonvinson@gmail.com>
2022-07-04 11:32:25 +0800

    main

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ window_clock = pygame.time.Clock()
 
 RUN = True
 
-tm = core.tilemap_manager.TilemapManager(map_1,tile)
+# tm = core.tilemap_manager.TilemapManager(map_1,tile)
 
 while RUN == True:
     window.fill((0,0,0,0))
@@ -31,16 +31,29 @@ while RUN == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             RUN = False
-        if event.type == pygame.MOUSEMOTION:
-            tm.touch(12)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            tm.touch(12)
+        # if event.type == pygame.MOUSEMOTION:
+        #     tm.touch(12)
+        # if event.type == pygame.MOUSEBUTTONDOWN:
+        #     tm.touch(12)
             
     tilemap_surface = pygame.Surface((window_size[0]/4,window_size[1]/4)).convert_alpha()
     tilemap_surface.fill((0,0,0,0))
     
-    tm.renderer(tilemap_surface)
+    # tm.renderer(tilemap_surface)
     # tm.timer()
+    
+    map_loop = map_1
+    
+    for y in map_loop:
+        for x in range(len(map_loop[y])):
+            for z in range(len(map_loop[y][x])):
+                tile_pos = {'x':x,'y':y,'z':z}
+                tile_code = map_loop[y][x][z]
+                # t = core.tile.Tile(tile_pos,tile_code)
+                print(tile_pos['y'])
+                
+                
+    
     
     tilemap_surface = pygame.transform.scale(tilemap_surface,window_size)
     window.blit(tilemap_surface, (0,0))
